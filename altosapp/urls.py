465ae02359f5ef02a4da8cd10329187ac02ad335
abhinandtk-1998 <abhinandchampad76@gmail.com>
2024-02-14 17:28:53 +0530

    second commit

--- altosapp/urls.py
+++ altosapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path,include
 from .import views
 
 urlpatterns = [
-    path('first-page/', views.first_page, name='first_page'),
-    path('second-page/',views.second_page,name='second_page'),
-    path('move-to-basket/', views.move_to_basket, name='move_to_basket'),
+    path('', views.first_page, name='first_page'),
+    path('second_page', views.second_page, name='second_page'),
 ]
--- altosapp/views.py
+++ altosapp/views.py
@@ -4,20 +4,11 @@ from django.shortcuts import render
 
 from django.shortcuts import render
 from django.http import HttpResponseRedirect
-from .models import Basket, Item
 
 def first_page(request):
     return render(request, 'first_page.html')
 
 def second_page(request):
-    return render(request, 'second_page.html')
-
-def move_to_basket(request):
-    if request.method == 'POST':
-        item_id = request.POST.get('item_id')
-        # Update database: move item to basket
-        return HttpResponseRedirect('/second-page/')  # Redirect to second page
-    else:
-        return HttpResponseRedirect('/first-page/')  # Redirect back if not a POST request
-
+    item_id = request.GET.get('item_id')
+    return render(request, 'second_page.html', {'item_id': item_id})
 
