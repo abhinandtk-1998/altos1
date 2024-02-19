97b17240ec2470abb770d63a5ee96368dbcda167
abhinandtk-1998 <abhinandchampad76@gmail.com>
2024-02-19 23:32:38 +0530

    sixth commit

--- altosapp/urls.py
+++ altosapp/urls.py
@@ -4,5 +4,4 @@ from .import views
 urlpatterns = [
     path('', views.first_page, name='first_page'),
     path('second_page/', views.second_page, name='second_page'),
-    path('about_us',views.about_us,name='about_us'),
 ]
--- altosapp/views.py
+++ altosapp/views.py
@@ -3,13 +3,11 @@ from django.shortcuts import render
 # Create your views here.
 
 def first_page(request):
-    item_idf = request.GET.get('item_idf', '').split(',')
+    item_idf = request.GET.get('item_idf',' ').split(',')
     return render(request, 'first_page.html',{'item_idf': item_idf})
 
 def second_page(request):
-    item_ids = request.GET.get('item_ids', '').split(',')
+    item_ids = request.GET.get('item_ids',' ').split(',')
     return render(request, 'second_page.html', {'item_ids': item_ids})
 
-def about_us(request):
-    return render(request, 'about_us.html')
 
