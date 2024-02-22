22a90446acd56fa2f47bca40b069216a4cde87b7
abhinandtk-1998 <abhinandchampad76@gmail.com>
2024-02-22 23:40:35 +0530

    ninth commit

--- altosapp/urls.py
+++ altosapp/urls.py
@@ -2,6 +2,8 @@ from django.urls import path,include
 from .import views
 
 urlpatterns = [
-    path('', views.first_page, name='first_page'),
-    path('second_page/', views.second_page, name='second_page'),
+    # path('', views.first_page, name='first_page'),
+    # path('second_page/', views.second_page, name='second_page'),
+    path('', views.page1, name='page1'),
+
 ]
--- altosapp/views.py
+++ altosapp/views.py
@@ -2,12 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def first_page(request):
-    item_idf = request.GET.get('item_idf',' ').split(',')
-    return render(request, 'first_page.html',{'item_idf': item_idf})
+# def first_page(request):
+#     item_idf = request.GET.get('item_idf',' ').split(',')
+#     return render(request, 'first_page.html',{'item_idf': item_idf})
 
-def second_page(request):
-    item_ids = request.GET.get('item_ids',' ').split(',')
-    return render(request, 'second_page.html', {'item_ids': item_ids})
+# def second_page(request):
+#     item_ids = request.GET.get('item_ids',' ').split(',')
+#     return render(request, 'second_page.html', {'item_ids': item_ids})
+
+def page1(request):
+    return render(request, 'page1.html')
 
 
