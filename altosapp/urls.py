37006ea8742ed7dac3a017e21d445f015a98f34d
abhinandtk-1998 <abhinandchampad76@gmail.com>
2024-02-23 18:54:12 +0530

    tenth commit

--- altosapp/urls.py
+++ altosapp/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     # path('', views.first_page, name='first_page'),
     # path('second_page/', views.second_page, name='second_page'),
     path('', views.page1, name='page1'),
+    path('page2',views.page2, name='page2'),
 
 ]
--- altosapp/views.py
+++ altosapp/views.py
@@ -6,11 +6,17 @@ from django.shortcuts import render
 #     item_idf = request.GET.get('item_idf',' ').split(',')
 #     return render(request, 'first_page.html',{'item_idf': item_idf})
 
-# def second_page(request):
-#     item_ids = request.GET.get('item_ids',' ').split(',')
-#     return render(request, 'second_page.html', {'item_ids': item_ids})
+def second_page(request):
+    item_ids = request.GET.get('item_ids',' ').split(',')
+    return render(request, 'second_page.html', {'item_ids': item_ids})
 
 def page1(request):
-    return render(request, 'page1.html')
+    item_idf = request.GET.get('item_idf',' ').split(',')
+    return render(request, 'page1.html',{'item_idf':item_idf})
+
+def page2(request):
+    item_ids = request.GET.get('item_ids',' ').split(',')
+    return render(request, 'page2.html', {'item_ids': item_ids})
+
 
 
